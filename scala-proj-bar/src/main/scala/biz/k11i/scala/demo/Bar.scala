7cd0aedda8ad175dfc9ea0e5e0fb39178603a116
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2017-02-19 03:38:34 +0900

    Add scala-resolver and its sample

--- scala-proj-bar/src/main/scala/biz/k11i/scala/demo/Bar.scala
+++ scala-proj-bar/src/main/scala/biz/k11i/scala/demo/Bar.scala
@@ -1,7 +1,10 @@
 package biz.k11i.scala.demo
 
+import org.json4s.native.JsonMethods
+
 object Bar {
   def main(args: Array[String]): Unit = {
     println("Bar")
+    println(JsonMethods.parse("""{"hello":"world"}"""))
   }
 }
